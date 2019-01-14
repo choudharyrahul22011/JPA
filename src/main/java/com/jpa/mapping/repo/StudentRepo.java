89ca216080027168c31bd51d0d2725cd88facf51
choudharyrahul22011 <choudharyRahul2201@gmail.com>
2019-01-14 11:26:21 +0530

    exception handling added

--- src/main/java/com/jpa/mapping/repo/StudentRepo.java
+++ src/main/java/com/jpa/mapping/repo/StudentRepo.java
@@ -1,6 +1,7 @@
 package com.jpa.mapping.repo;
 
 import com.jpa.mapping.entity.Student;
+import com.jpa.mapping.exception.StudentNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -15,7 +16,10 @@ public class StudentRepo {
 
     public Student find(long id) {
         Student student = entityManager.find(Student.class, id);
-        System.out.println(student);
+        System.out.println("Student : " + student);
+        if(student == null){
+            throw new StudentNotFoundException("Student not found with ID : " + id);
+        }
         System.out.println(student.getCourses());
         return student;
     }
--- src/main/java/com/jpa/mapping/resource/StudentResource.java
+++ src/main/java/com/jpa/mapping/resource/StudentResource.java
@@ -1,11 +1,16 @@
 package com.jpa.mapping.resource;
 
 import com.jpa.mapping.entity.Student;
+import com.jpa.mapping.exception.StudentNotFoundException;
 import com.jpa.mapping.repo.StudentRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import com.jpa.mapping.exception.ErrorResponse;
 
 @RestController
 public class StudentResource {
@@ -15,6 +20,16 @@ public class StudentResource {
 
     @GetMapping("/student/{id}")
     public Student find(@PathVariable long id){
-        return studentRepo.find(id);
+        Student student = studentRepo.find(id);
+        return student;
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleException(StudentNotFoundException ex){
+        ErrorResponse response = new ErrorResponse();
+        response.setStatus(HttpStatus.NOT_FOUND.value());
+        response.setMessage(ex.getMessage());
+        response.setTimeStamp(System.currentTimeMillis());
+        return new ResponseEntity<>(response,HttpStatus.NOT_FOUND);
     }
 }
